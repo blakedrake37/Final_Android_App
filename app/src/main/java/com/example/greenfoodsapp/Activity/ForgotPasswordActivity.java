916a4ea5da79d6a528f1d467c90afc9215dbca5a
blakedrake37 <142980009+blakedrake37@users.noreply.github.com>
2024-05-31 23:49:28 +0700

    ok
    ok

---
+++ app/src/main/java/com/example/greenfoodsapp/Activity/ForgotPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.example.greenfoodsapp.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.greenfoodsapp.R;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
---
+++ app/src/main/java/com/example/greenfoodsapp/Adapter/ProductAdapter_tabLayout.java
@@ -0,0 +1,32 @@
+package com.example.greenfoodsapp.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.greenfoodsapp.Fragment.ProductFragments.FoodFragment;
+import com.example.greenfoodsapp.Fragment.ProductFragments.FruitFragment;
+import com.example.greenfoodsapp.Fragment.ProductFragments.MeatFragment;
+import com.example.greenfoodsapp.Fragment.ProductFragments.VegetableFragment;
+
+public class ProductAdapter_tabLayout extends FragmentStateAdapter {
+    public ProductAdapter_tabLayout(@NonNull Fragment fragment) {
+        super(fragment);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0: return new VegetableFragment();
+            case 1: return new FruitFragment();
+            case 2: return new MeatFragment();
+            default: return new FoodFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+}
